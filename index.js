c31af649fc25240a28965813f2a0190f06cdcf88
Kevin <39208150+Kvrnn@users.noreply.github.com>
2024-04-22 20:03:50 -0700

    garbled mess

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+/*!
+ * zohome-nodejs-sdk
+ * Copyright(c) 2024 Kevin R
+ * GPL-3.0 Licensed
+ */
+
+/**
+ * Change Log:
+ *
+ * v0.0.1 - 4/22/2024:
+ * I'M ALIVE
+ */
+
+module.exports = require('./lib/index');
