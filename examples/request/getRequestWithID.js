17fb94d8befd5b67868bcd0d4bb20ec026bd6b67
Kevin <39208150+Kvrnn@users.noreply.github.com>
2024-04-24 00:10:26 -0700

    garbled mess

---
+++ examples/request/getRequestWithID.js
@@ -0,0 +1,6 @@
+import { ManageEngine } from "../../lib/index";
+
+// Will attempt to establish a connection with the ManageEngine API using the provided credentials
+// First will create a new instance of the ManageEngine class
+ManageEngine.initialize("{client_id}", "{client_secret}", "{refresh_token}", "{base_url}");
+
